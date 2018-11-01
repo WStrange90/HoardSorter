cf063f971f7f19e8cf3a8ff526031a4da57714d1
SeanNJones <43797349+SeanNJones@users.noreply.github.com>
2018-11-01 17:38:44 -0400

    Registration Phone Number Controller

--- HoardSorter/HoardSorter/Controllers/AccountController.cs
+++ HoardSorter/HoardSorter/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace HoardSorter.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, PhoneNumber = model.Phone };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
