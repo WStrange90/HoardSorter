b9e978355109be2ce22771fc1c2a6c92c20a023a
WStrange90 <williamstrange90@gmail.com>
2018-11-02 22:07:33 -0400

    New VS project
    Includes scaffolding. Fixed a connection string error.

---
+++ HoardSorter/HoardSorter/Controllers/CollectionsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HoardSorter.Models;
+
+namespace HoardSorter.Controllers
+{
+    public class CollectionsController : Controller
+    {
+        private HoardSorterEntities db = new HoardSorterEntities();
+
+        // GET: Collections
+        public ActionResult Index()
+        {
+            var collections = db.Collections.Include(c => c.AspNetUsers);
+            return View(collections.ToList());
+        }
+
+        // GET: Collections/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Collections collections = db.Collections.Find(id);
+            if (collections == null)
+            {
+                return HttpNotFound();
+            }
+            return View(collections);
+        }
+
+        // GET: Collections/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Collections/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "collectorID,UserID")] Collections collections)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Collections.Add(collections);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", collections.UserID);
+            return View(collections);
+        }
+
+        // GET: Collections/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Collections collections = db.Collections.Find(id);
+            if (collections == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", collections.UserID);
+            return View(collections);
+        }
+
+        // POST: Collections/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "collectorID,UserID")] Collections collections)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(collections).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", collections.UserID);
+            return View(collections);
+        }
+
+        // GET: Collections/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Collections collections = db.Collections.Find(id);
+            if (collections == null)
+            {
+                return HttpNotFound();
+            }
+            return View(collections);
+        }
+
+        // POST: Collections/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Collections collections = db.Collections.Find(id);
+            db.Collections.Remove(collections);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HoardSorter/HoardSorter/Controllers/DecksController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HoardSorter.Models;
+
+namespace HoardSorter.Controllers
+{
+    public class DecksController : Controller
+    {
+        private HoardSorterEntities db = new HoardSorterEntities();
+
+        // GET: Decks
+        public ActionResult Index()
+        {
+            var deck = db.Deck.Include(d => d.AspNetUsers).Include(d => d.DeckType);
+            return View(deck.ToList());
+        }
+
+        // GET: Decks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deck deck = db.Deck.Find(id);
+            if (deck == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deck);
+        }
+
+        // GET: Decks/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.DeckTypeID = new SelectList(db.DeckType, "DeckTypeID", "DeckType1");
+            return View();
+        }
+
+        // POST: Decks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DeckID,UserID,DeckTypeID,DeckName")] Deck deck)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Deck.Add(deck);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", deck.UserID);
+            ViewBag.DeckTypeID = new SelectList(db.DeckType, "DeckTypeID", "DeckType1", deck.DeckTypeID);
+            return View(deck);
+        }
+
+        // GET: Decks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deck deck = db.Deck.Find(id);
+            if (deck == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", deck.UserID);
+            ViewBag.DeckTypeID = new SelectList(db.DeckType, "DeckTypeID", "DeckType1", deck.DeckTypeID);
+            return View(deck);
+        }
+
+        // POST: Decks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DeckID,UserID,DeckTypeID,DeckName")] Deck deck)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(deck).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", deck.UserID);
+            ViewBag.DeckTypeID = new SelectList(db.DeckType, "DeckTypeID", "DeckType1", deck.DeckTypeID);
+            return View(deck);
+        }
+
+        // GET: Decks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deck deck = db.Deck.Find(id);
+            if (deck == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deck);
+        }
+
+        // POST: Decks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Deck deck = db.Deck.Find(id);
+            db.Deck.Remove(deck);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
