6952c45c2bf07f3e792aaf67b44ff012a505f0ff
EvanDonald <43646012+EvanDonald@users.noreply.github.com>
2018-11-03 10:08:35 -0400

    Set login to be default page. Added trade.cs and trade views

---
+++ HoardSorter/HoardSorter/Models/Trade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HoardSorter.Models
+{
+    public class Trade
+    {
+        [Key]
+        public int id { get; set; }
+    }
+}
